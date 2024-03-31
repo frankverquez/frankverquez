b05b2dd7dbb7cfbdcfd9f3f3483182b2cbaf12b7
FRANK VERQUEZ <saputrafrengky641@gmail.com>
2024-03-31 23:03:00 +0700

    tugas 4 pimk

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./tailwind-components/*.{html,js}",
+    "./regular-expressions/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
